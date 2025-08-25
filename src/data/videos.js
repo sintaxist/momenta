d72438d78253dcfd7192941e150e895cb9f233eb
Alejandro Canek <alejandro.canek.dev@gmail.com>
2025-08-25 08:08:21 -0600

    feat: add video SEO

--- src/data/videos.js
+++ src/data/videos.js
@@ -5,6 +5,7 @@ export const projectData = [
       hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093433/Boda_Roma_ten5c0.m3u8",
       mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093433/Boda_Roma_ten5c0.mp4"
     },
+    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093433/Boda_Roma_ten5c0.jpg",
     demoUrl: "#" 
   },
   { 
@@ -13,6 +14,7 @@ export const projectData = [
       hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093813/Astom_Summit_2024_fttiw2.m3u8",
       mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093813/Astom_Summit_2024_fttiw2.mp4"
     },
+    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093813/Astom_Summit_2024_fttiw2.jpg",
     demoUrl: "#" 
   },
   { 
@@ -21,6 +23,7 @@ export const projectData = [
       hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.m3u8",
       mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.mp4"
     },
+    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756098681/Boda_Jimena_y_Canek_pvoexk.jpg",
     demoUrl: "#" 
   }
 ];
--- src/types/portfolio.ts
+++ src/types/portfolio.ts
@@ -6,3 +6,12 @@ export interface projectType {
   description: string;
   features: string[];
 }
+
+export type projectVideo = {
+  title: string;
+  video: { hls: string; mp4: string };
+  poster?: string;
+  demoUrl: string;
+  uploadDate?: string;
+  description?: string;
+};
