0f0991abcb79691f982f48d8012a2d59793561a1
Alejandro Canek <alejandro.canek.dev@gmail.com>
2025-08-25 08:27:24 -0600

    fix: Optimize Mockup

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,29 +1,29 @@
-export const projectData = [
-  { 
-    title: "Boda María y José", 
-    video: {
-      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093433/Boda_Roma_ten5c0.m3u8",
-      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093433/Boda_Roma_ten5c0.mp4"
+  export const videoData = [
+    { 
+      title: "Boda María y José", 
+      video: {
+        hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093433/Boda_Roma_ten5c0.m3u8",
+        mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093433/Boda_Roma_ten5c0.mp4"
+      },
+      poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093433/Boda_Roma_ten5c0.jpg",
+      demoUrl: "#" 
     },
-    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093433/Boda_Roma_ten5c0.jpg",
-    demoUrl: "#" 
-  },
-  { 
-    title: "Astom Summit 2024", 
-    video: {
-      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093813/Astom_Summit_2024_fttiw2.m3u8",
-      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093813/Astom_Summit_2024_fttiw2.mp4"
+    { 
+      title: "Astom Summit 2024", 
+      video: {
+        hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093813/Astom_Summit_2024_fttiw2.m3u8",
+        mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093813/Astom_Summit_2024_fttiw2.mp4"
+      },
+      poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093813/Astom_Summit_2024_fttiw2.jpg",
+      demoUrl: "#" 
     },
-    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756093813/Astom_Summit_2024_fttiw2.jpg",
-    demoUrl: "#" 
-  },
-  { 
-    title: "Boda Jimena y Canek", 
-    video: {
-      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.m3u8",
-      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.mp4"
-    },
-    poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756098681/Boda_Jimena_y_Canek_pvoexk.jpg",
-    demoUrl: "#" 
-  }
-];
+    { 
+      title: "Boda Jimena y Canek", 
+      video: {
+        hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.m3u8",
+        mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.mp4"
+      },
+      poster: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/so_2/v1756098681/Boda_Jimena_y_Canek_pvoexk.jpg",
+      demoUrl: "#" 
+    }
+  ];
