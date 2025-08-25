41892ac145e0499ed4faeab3bbe126f5f31d680a
Alejandro Canek <alejandro.canek.dev@gmail.com>
2025-08-25 07:41:10 -0600

    fix: upload videos

---
+++ src/data/videos.js
@@ -0,0 +1,26 @@
+export const projectData = [
+  { 
+    title: "Boda María y José", 
+    video: {
+      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093433/Boda_Roma_ten5c0.m3u8",
+      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093433/Boda_Roma_ten5c0.mp4"
+    },
+    demoUrl: "#" 
+  },
+  { 
+    title: "Astom Summit 2024", 
+    video: {
+      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756093813/Astom_Summit_2024_fttiw2.m3u8",
+      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756093813/Astom_Summit_2024_fttiw2.mp4"
+    },
+    demoUrl: "#" 
+  },
+  { 
+    title: "Boda Jimena y Canek", 
+    video: {
+      hls: "https://res.cloudinary.com/dxdysqkbg/video/upload/sp_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.m3u8",
+      mp4: "https://res.cloudinary.com/dxdysqkbg/video/upload/f_auto,q_auto/v1756098681/Boda_Jimena_y_Canek_pvoexk.mp4"
+    },
+    demoUrl: "#" 
+  }
+];
