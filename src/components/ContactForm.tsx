8c8a369c8106d0f45061d6694bdc1fa1d7fea935
Alejandro Canek Fernández Maya <77036251+sintaxist@users.noreply.github.com>
2025-09-10 11:23:47 -0600

    fix: set new font size form for accesibility

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -109,7 +109,7 @@ export function ContactForm() {
   };
 
   const inputStyles =
-    "flex h-11 w-full rounded-md border border-gray-300 bg-white/60 px-3 py-2 text-md font-sora ring-offset-background placeholder:text-gray-400 focus:border-indigo-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
+    "flex h-11 w-full rounded-md border border-gray-300 bg-white/60 px-3 py-2 text-[15px] font-sora ring-offset-background placeholder:text-gray-400 focus:border-indigo-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
 
   return (
     <>
